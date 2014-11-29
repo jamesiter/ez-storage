35788d5e90473cf7f85ec0fe9413d7f741df4b15
James Iter <james.iter.cn@gmail.com>
2014-11-30 01:14:21 +0800

    add test code for upload file

--- hello.py
+++ hello.py
@@ -1,9 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/files/add', methods=['GET', 'POST'])
+def receive_upload_files():
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename[file.filename]
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
